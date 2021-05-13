891c831d50cc85d009aa1f87acc67039c3ae87f2
Henrique Gogó <henriquegogo@gmail.com>
2021-05-13 00:39:39 -0300

    Calling service worker

--- videogrid/serviceWorker.js
+++ videogrid/serviceWorker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
-      return cache.addAll(['favicon.ico']);
+      return cache.addAll([]);
     })
   );
 });

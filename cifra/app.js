4d4e8d602705aea7f9b023b384ef686f92193401
Henrique Gogó <henriquegogo@gmail.com>
2023-11-14 14:17:23 -0300

    Cifrador

---
+++ cifra/app.js
@@ -0,0 +1,13 @@
+(function() {
+  const keys = document.getElementsByTagName("td");
+  for (let key of keys) {
+    key.onclick = function() {
+      if (key.dataset.value == "backspace") {
+        content.value = content.value.slice(0, -1);
+      } else {
+        content.value += key.dataset.value || key.innerText;
+      }
+      content.focus();
+    }
+  }
+})();
